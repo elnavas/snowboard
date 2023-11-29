974fdb385518772b91a75e20354ad0c7968e4646
elnavas <carlsnavarr@gmail.com>
2023-11-29 13:25:39 -0600

    Added scene loader

--- Assets/Scripts/DetectorDeChoque.cs
+++ Assets/Scripts/DetectorDeChoque.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DetectorDeChoque : MonoBehaviour
 {
+    [SerializeField] float delayTime = 1;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Suelo"){
-            Debug.Log("Me cai");
+            Invoke("ReloadScene", delayTime);
         }
+    }
+    void ReloadScene(){
+        SceneManager.LoadScene("PrimerNivel");
     }
 }
--- Assets/Scripts/Meta.cs
+++ Assets/Scripts/Meta.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Meta : MonoBehaviour
 {
+    [SerializeField] float delayTime = 1;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Jugador"){
-            Debug.Log("Terminasta Pa");
+            Invoke("ReloadScene", delayTime);
+            
         }
     }
+
+    void ReloadScene(){
+        SceneManager.LoadScene("PrimerNivel");
+    }
+
 }
