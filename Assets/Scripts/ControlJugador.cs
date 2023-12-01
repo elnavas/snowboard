a0d9bc895789bc1e6285439337650fdcfd99fe09
elnavas <carlsnavarr@gmail.com>
2023-12-01 12:42:34 -0600

    Added Boost

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,33 @@ public class ControlJugador : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 1;
+    [SerializeField] float boostSpeed = 30;
+    [SerializeField] float baseSpeed = 20;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)){
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer(){
         if(Input.GetKey(KeyCode.LeftArrow)){
             rb2d.AddTorque(torqueAmount);
         }
